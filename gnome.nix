3d1e17a9fcb72e08d28a44427182130a99c581a8
toast003 <39011842+toast003@users.noreply.github.com>
2021-12-24 16:18:05 +0100

    Replace ddterm with guake, added meld

--- gnome.nix
+++ gnome.nix
@@ -18,6 +18,18 @@
 		# Set "Menu" key to toggle ddterm
 		[com.github.amezin.ddterm]
 		ddterm-toogle-hotkey=['Menu']
+
+		[apps.guake.general]
+		gtk-prefer-dark-theme=true
+		start-at-login=true
+		use-audible-bell=true
+		
+		[apps.guake.keybindings.global]
+		show-hide='Menu'
+
+		[apps.guake.style.font]
+		palette='#000027273131:#D0D01B1B2424:#6B6BBEBE6C6C:#A5A577770505:#20207575C7C7:#C6C61B1B6E6E:#252591918585:#E9E9E2E2CBCB:#000063638888:#F4F415153B3B:#5050EEEE8484:#B1B17E7E2828:#17178D8DC7C7:#E1E14D4D8E8E:#0000B2B29E9E:#FCFCF4F4DCDC:#9B9BC1C1C2C2:#00001E1E2626'
+		pallete-name='Solarized Dark Higher Contrast'
   	'';
   	 extraGSettingsOverridePackages = [
   	 	pkgs.gsettings-desktop-schemas
@@ -29,9 +41,8 @@
   services.gnome.gnome-keyring.enable = pkgs.lib.mkForce false; # For some reason if you just set it to false it errors out.
   environment.systemPackages = with pkgs; [
 	# Gnome extensions.
-	gnomeExtensions.ddterm
-	gjs # For some reason gjs in not installed by default, and ddterm needs it
-	gnomeExtensions.jiggle
+	#gnomeExtensions.ddterm # The package for ddterm is broken at the time of writing (24/12/2021)
+	gjs # Ddterm requires gjs, but since the package seems to be broken it's not actually needed. I'll keep it here just in case ;)
 	gnomeExtensions.appindicator
 	gnomeExtensions.better-osd-gnome-40
 	gnomeExtensions.blur-my-shell
@@ -53,6 +64,7 @@
 	gnome.dconf-editor
   	gnome.file-roller
   	evince
+  	meld
   ];
   
 }
