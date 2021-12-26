4a0aca3b021af78c1073503d4d08776d6fc779ae
toast003 <39011842+toast003@users.noreply.github.com>
2021-12-26 23:06:55 +0100

    Fix formatting

--- configuration.nix
+++ configuration.nix
@@ -7,8 +7,8 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./gnome.nix
+  	./hardware-configuration.nix
+  	./gnome.nix
     ];
 
   # Boot loader settings.
